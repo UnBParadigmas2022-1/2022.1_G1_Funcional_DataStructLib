d7654ec2468f44b44a8aa77997b9e9500519e3d0
Gabriel Albino <enggabrielalbino@gmail.com>
2022-07-03 19:16:32 -0300

    Removendo duplicação de código e incrementando readme

--- Grafos/Kruskals.hs
+++ Grafos/Kruskals.hs
@@ -1,38 +1,8 @@
 import Data.Maybe
 import Data.List
 import qualified Data.Map as Map
+import WeightedGraph
 
-type Graph = Map.Map String (Map.Map String Double)
-
-uniq :: [String] -> [String]
-uniq lst = head lst : map snd (Data.List.filter fst (Data.List.zipWith cmpNbrs lst (tail lst)))
-  where cmpNbrs x y = (x /= y, y)
-
-makeGraph :: [(String, String, Double)] -> Graph
-makeGraph edges = Map.fromList (map makeNode keys)
-  where
-    keys = uniq (Data.List.sort (map (\(x,y,z)->x) edges ++ map (\(x,y,z)->y) edges))
-
-    findEdgesFrom n [] = []
-    findEdgesFrom n ((s, t, w) : rest)
-      | n == s    = (t, w) : findEdgesFrom n rest
-      | otherwise = findEdgesFrom n rest
-
-    makeNode n = (n, Map.fromList (findEdgesFrom n edges))
-
-weight :: Graph -> String -> String -> Double
-weight g s t = Map.findWithDefault 1e100 t smap
-  where smap = Map.findWithDefault Map.empty s g
-
-nodes :: Graph -> [String]
-nodes g = Map.keys g
-
-neighbors :: Graph -> String -> [String]
-neighbors g n = Map.keys (Map.findWithDefault Map.empty n g)
-
-ark = makeGraph (undirEdges ++ map (\(s, t, w) -> (t, s, w)) undirEdges)
-  where undirEdges = [("Conway", "Russellville", 44.2),("Conway", "Clinton", 38.2),("Conway", "Heber Springs", 39.0),("Heber Springs", "Mountain View", 41.9),("Clinton", "Mountain View", 34.0),("Clinton", "Marshall", 26.1),("Marshall", "Mountain View", 37.1),("Marshall", "Yellville", 34.1),("Yellville", "Mountain Home", 18.7),("Mountain View", "Mountain Home", 47.3),("Marshall", "Pindall", 20.5),("Pindall", "Yellville", 19.7), ("Russellville", "Pindall", 78.3),("Russellville", "Marshall", 72.0)]
-  
 data Aresta = Aresta String String Double deriving (Show)  
 
 data Subset = Subset Int Int deriving (Show)
