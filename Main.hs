5e43f412fa30a7ae2732a5ce2b4c9d97abd56fe9
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2022-07-04 18:26:28 -0300

    Add functions at main

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import TimeIt
 import TesteSort(doMergeSorts, doQuicksort, doInsertion, doSelectionSort, doBubbleSort, allSort ) 
+import TesteFila (testeCriaLista, testeAdicionaElemento, testeRemoveElemento, testeReverse, testeMaiorValor)
 import WeightedGraph(ark, big)
 import BellmanFord(bellmanFord)
 import Kruskals(kruskals)
@@ -38,18 +39,36 @@ menuSort = do
         "5" -> timeIt $doBubbleSort
         "6" -> timeIt $allSort
 
+menuLista = do   
+    putStrLn "Módulos:"
+    putStrLn "(1) Criar uma lista com 1000 elementos"
+    putStrLn "(2) Adiciona um valor a lista"
+    putStrLn "(3) Remove último elemento da lista"
+    putStrLn "(4) Implementa reverse na lista"
+    putStrLn "(5) Pegar maior valor da lista"
+    putStr "Selecione o módulo desejado: "
+    opcao <- getLine
+    case opcao of
+        "1" -> timeIt $testeCriaLista
+        "2" -> timeIt $testeAdicionaElemento
+        "3" -> timeIt $testeRemoveElemento
+        "4" -> timeIt $testeReverse
+        "5" -> timeIt $testeMaiorValor
+
 main = do
   putStrLn "Módulos:"
   putStrLn "(1) Grafos"
   putStrLn "(2) Árvore Binária"
   putStrLn "(3) Pilha"
-  putStrLn "(4) Merge Sort"
-  putStrLn "(5) Algoritimos de Ordenação"
+  putStrLn "(4) Lista"
+  putStrLn "(5) Merge Sort"
+  putStrLn "(6) Algoritimos de Ordenação"
   putStr "Selecione o módulo desejado: "
   opcao <- getLine
   case opcao of
     "1" -> menuGrafo
     "2" -> timeIt $ putStrLn ("Result: " ++ show (valuesPostOrder (insertElement treexample 5)))
     "3" -> timeIt $ testePilha
-    "4" -> timeIt $ doMergeSort
-    "5" -> menuSort
+    "4" -> menuLista
+    "5" -> timeIt $ doMergeSort
+    "6" -> menuSort
