68d3964a40d3dce3121cfd5850daace085b36ba4
Gabriel Albino <enggabrielalbino@gmail.com>
2022-07-04 00:35:46 -0300

    Adicionando mergesort ao menu

--- MergeSort/Teste.hs
+++ MergeSort/Teste.hs
@@ -4,11 +4,6 @@ import System.IO
 import Data.Char
 import MergeSort (mergeSort, divide, merge)
 
-teste1 = do 
-    valores <- readFile "100000.txt"
-    print (mergeSort (map read $ words valores :: [Int]))
-
-
-teste2 = do 
-    valores <- readFile "10000.txt"
+doMergeSort = do 
+    valores <- readFile "./MergeSort/100000.txt"
     print (mergeSort (map read $ words valores :: [Int]))
