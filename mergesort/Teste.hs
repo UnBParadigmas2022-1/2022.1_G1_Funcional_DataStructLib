11423052f0a93639de275cfe32c038f07cf9c026
Bruno Nunes <brunocmo.nunes@gmail.com>
2022-07-04 21:59:25 -0300

    Fix pathfile value

--- mergesort/Teste.hs
+++ mergesort/Teste.hs
@@ -5,5 +5,5 @@ import Data.Char
 import MergeSort (mergeSort, divide, merge)
 
 doMergeSort = do 
-    valores <- readFile "./MergeSort/100000.txt"
+    valores <- readFile "./mergesort/100000.txt"
     print (mergeSort (map read $ words valores :: [Int]))
